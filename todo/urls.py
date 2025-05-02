4afa8737d52af9bb184a05988bf202b1769b0bd2
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-05-02 19:04:07 +0530

    refactor: simplify URL patterns by consolidating todo views into a single TodoAPIView

--- todo/urls.py
+++ todo/urls.py
@@ -1,17 +1,7 @@
 from django.urls import path
-from .views import (
-    TodoCreateAPIView,
-    TodoListAPIView,
-    TodoUpdateAPIView,
-    TodoDeleteAPIView,
-    # TodoPartialUpdateAPIVIew
-)
+from .views import TodoAPIView
 
 urlpatterns = [
-    path('todos/', TodoListAPIView.as_view(), name='all-todos'),
-    path('todo/<int:pk>', TodoListAPIView.as_view(), name='view-todo'),
-    path('todo/create/', TodoCreateAPIView.as_view(), name='create-todo'),
-    path('todo/update/<int:pk>', TodoUpdateAPIView.as_view(), name='update-todo'),
-    # path('todo/part_update/<int:pk>', TodoPartialUpdateAPIVIew.as_view(), name='part-update-todo'),
-    path('todo/delete/<int:pk>', TodoDeleteAPIView.as_view(), name='delete-todo'),
+    path('todo/', TodoAPIView.as_view(), name='all-todos-list'),
+    path('todo/<int:pk>', TodoAPIView.as_view(), name='todo-detail'),
 ]
