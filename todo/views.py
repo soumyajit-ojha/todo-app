b04c7936893a47b15e855231e0443cadceb6e823
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-05-02 19:03:39 +0530

    refactor: unify multiple Todo views into a single TodoAPIView.

--- todo/views.py
+++ todo/views.py
@@ -9,43 +9,39 @@ from .serializers import TodoSerializer
 
 
     
-class TodoListAPIView(APIView):
+class TodoAPIView(APIView):
     """
-    A simple API view to for all todos.
+    A simple API view to for all todos and any specific todo.
+    Depending upon path parameter. If no parameter return all todos, 
+    if any specif id return specific todo.
     """
+
     def get(self, request, pk=None, format=None):
         if pk is not None:
-            # DETAIL
             todo       = get_object_or_404(Todo, pk=pk)
             serializer = TodoSerializer(todo)
-            return Response(serializer.data, status=200)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        # LIST
         queryset = Todo.objects.all()
         if not queryset:
-            return Response({"message":"No data found"}, status=204)
+            return Response({"message":"No data found"}, status=status.HTTP_204_NO_CONTENT)
 
         serializer = TodoSerializer(queryset, many=True)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    
-class TodoCreateAPIView(APIView):
-    """
-    APIView to create a new object of todo.
-    """
     def post(self, request: Request):
-        payload = request.data
-        serializer = TodoSerializer(data=payload)
-
+        """
+        APIView post method to create a new object of todo.
+        """
+        serializer = TodoSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-class TodoUpdateAPIView(APIView):
-    """
-    APIView for updating the todo completly.
-    """
+    
     def put(self, request: Request, pk: int):
+        """
+        APIView patch method to fully update a todo object.
+        """
         todo = get_object_or_404(Todo, pk=pk)
         serializer = TodoSerializer(instance=todo, data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -53,20 +49,22 @@ class TodoUpdateAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request: Request, pk: int):
+        """
+        APIView patch method to partialy update a todo object.
+        """
         todo = get_object_or_404(Todo, pk=pk)
         serializer = TodoSerializer(instance=todo, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-        
-class TodoDeleteAPIView(APIView):
-    """
-    APIView to delete an existing todo.
-    """
-
+       
     def delete(self, request: Request, pk:int):
+        """
+        APIView delete method to delete a todo object.
+        """
         todo = get_object_or_404(Todo, pk=pk)
         todo.delete()
+
         return Response({"message" : "No conttent found"}, status=status.HTTP_204_NO_CONTENT)
 
