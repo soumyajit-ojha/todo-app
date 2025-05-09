bfcc40ecd24aa79ff3480029479f31d1243dc87d
soumyajit-ojha <soumyajitojhag2@gmail.com>
2025-05-09 12:36:15 +0530

    changes: manual token generated, all profile api view created..

--- user/views.py
+++ user/views.py
@@ -1,37 +1,85 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+
+
 
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    ProfileSerializer,
+)
 
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    username_field = CustomUser.email
 
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        # (optional) add extra claims
-        token['username'] = user.username
-        return token
-    
+def get_token(user):
+    refresh = RefreshToken.for_user(user)
 
-class UserRegistration(APIView):
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token)
+    }
+
+class RegistrationAPIView(APIView):
     def post(self, request):
         try:
-            serializer = UserSerializer(data=request.data)
+            serializer = RegisterSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token = get_token(user)
+
+
+            return Response({"token":token, "messag":"Registeration successful."}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
             
-class LoginView(TokenObtainPairView):
-    """
-    request: { "email": "...", "password": "..." }
-    response: { "access": "...", "refresh": "..." }
-    """
-    serializer_class = CustomTokenObtainPairSerializer
+class UserLoginAPIView(APIView):
+    def post(self, request):
+        try:
+            serializer = LoginSerializer(data=request.data)
+
+            if serializer.is_valid(raise_exception=True):
+                email = serializer.data.get("email")
+                password = serializer.data.get("password")
+
+                user = authenticate(email=email, password=password)
+                token = get_token(user)
+
+                if not user:
+                    return Response({"message" : "email and password are not valid."}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response({"token":token, "message" : "Logged in successfully."}, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        except Exception as e:
+            return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            print(request.data)
+            serializer = ProfileSerializer(instance=request.user)
+            print(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class AllUserProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        try:
+            all_users = CustomUser.objects.all()
+            serializer = ProfileSerializer(all_users, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"errors": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
